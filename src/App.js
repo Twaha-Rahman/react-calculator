e3b62d7bdf73c6c3bedd053157d825fa22de5efc
Twaha-Rahman <mahi6703890@gmail.com>
2020-05-11 14:18:11 +0600

    Refactored for a clean start

--- src/App.js
+++ src/App.js
@@ -1,83 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   let [numberStr, setNumberStr] = useState('');
-  let [oparetor, setOparetor] = useState([]);
-  let [numbers, setNumbers] = useState([]);
-  let [lastClicked, setLastClicked] = useState('');
-  let [upperDisplay, setUpperDisplay] = useState('');
-  let [lowerDisplay, setLowerDisplay] = useState('');
-
-  useEffect(() => {
-    const arrToJoin = [];
-
-    if (numbers.length > oparetor.length) {
-      for (let index = 0; index < numbers.length; index++) {
-        if (numbers[index]) {
-          arrToJoin.push(numbers[index]);
-        }
-
-        if (oparetor[index]) {
-          arrToJoin.push(oparetor[index]);
-        }
-      }
-    } else {
-      for (let index = 0; index < oparetor.length; index++) {
-        if (numbers[index]) {
-          arrToJoin.push(numbers[index]);
-        }
-
-        if (oparetor[index]) {
-          arrToJoin.push(oparetor[index]);
-        }
-      }
-    }
-
-    if (numberStr !== '') {
-      arrToJoin.push(numberStr);
-    }
-
-    setUpperDisplay(arrToJoin.join(''));
-
-    if (numberStr.length > 9) {
-      setNumberStr('Digit Limit Reached!');
-    }
-  });
 
   return (
     <div className="calc-container">
       <div id="display">
         <div className="upper-display">
-          <p>{upperDisplay}</p>
+          <p>{'dasdasd'}</p>
         </div>
         <div className="lower-display">
-          <p>{numberStr || lowerDisplay}</p>
+          <p>{'sdasd'}</p>
         </div>
       </div>
       <button
         onClick={() => {
           setNumberStr('');
-          setOparetor([]);
-          setNumbers([]);
-          setUpperDisplay('');
-          setLowerDisplay('');
-          document.querySelector('#display').style.border = '';
         }}
         className="button ac-button"
+        id="clear"
       >
         AC
       </button>
       <button
         onClick={() => {
-          setOparetor([...oparetor, '/']);
-
-          const num = Number(numberStr);
-
-          if (num !== 0) {
-            setNumbers([...numbers, num]);
-            setNumberStr('');
-          }
+          setNumberStr(`${numberStr}/`);
         }}
         className="button operator"
         id="divide"
@@ -86,14 +34,7 @@ function App() {
       </button>
       <button
         onClick={() => {
-          setOparetor([...oparetor, '*']);
-
-          const num = Number(numberStr);
-
-          if (num !== 0) {
-            setNumbers([...numbers, num]);
-            setNumberStr('');
-          }
+          setNumberStr(`${numberStr}*`);
         }}
         className="button operator"
         id="multiply"
@@ -129,14 +70,7 @@ function App() {
       </button>
       <button
         onClick={() => {
-          setOparetor([...oparetor, '-']);
-
-          const num = Number(numberStr);
-
-          if (num !== 0) {
-            setNumbers([...numbers, num]);
-            setNumberStr('');
-          }
+          setNumberStr(`${numberStr}-`);
         }}
         className="button operator"
         id="subtract"
@@ -172,14 +106,7 @@ function App() {
       </button>
       <button
         onClick={() => {
-          setOparetor([...oparetor, '+']);
-
-          const num = Number(numberStr);
-
-          if (num !== 0) {
-            setNumbers([...numbers, num]);
-            setNumberStr('');
-          }
+          setNumberStr(`${numberStr}+`);
         }}
         className="button operator"
         id="add"
@@ -213,68 +140,7 @@ function App() {
       >
         3
       </button>
-      <button
-        onClick={() => {
-          const num = Number(numberStr);
-
-          let extendedNumbers;
-
-          if (num !== 0) {
-            setNumbers([...numbers, num]);
-            extendedNumbers = [...numbers, num];
-          } else {
-            extendedNumbers = [...numbers];
-          }
-
-          setNumberStr('');
-
-          if (extendedNumbers.length - 1 !== oparetor.length) {
-            document.querySelector('#display').style.border = 'orangered 0.5px solid';
-          } else {
-            console.log(extendedNumbers, oparetor);
-            let result = extendedNumbers[0];
-
-            extendedNumbers.forEach((val, index) => {
-              const runningOperator = oparetor[index];
-              if (runningOperator === '+') {
-                if (extendedNumbers[index + 1]) {
-                  result += extendedNumbers[index + 1];
-                }
-              }
-              if (runningOperator === '-') {
-                if (extendedNumbers[index + 1]) {
-                  result -= extendedNumbers[index + 1];
-                }
-              }
-              if (runningOperator === '*') {
-                if (extendedNumbers[index + 1]) {
-                  result *= extendedNumbers[index + 1];
-                }
-              }
-              if (runningOperator === '/') {
-                if (extendedNumbers[index + 1]) {
-                  result /= extendedNumbers[index + 1];
-                }
-              }
-            });
-
-            result = parseFloat(result.toFixed(2));
-
-            console.log(result);
-
-            setLowerDisplay(result);
-
-            setNumbers([result]);
-            setOparetor([]);
-            setUpperDisplay('');
-            setLowerDisplay('');
-
-            document.querySelector('#display').style.border = '';
-          }
-        }}
-        className="button operator"
-        id="equals"
-      >
+      <button onClick={() => {}} className="button operator" id="equals">
         =
       </button>
       <button
