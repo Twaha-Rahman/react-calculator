dba806fde98286a4f1dcda780ba956d023470a7c
Twaha-Rahman <mahi6703890@gmail.com>
2020-05-12 11:05:24 +0600

    FIxed view logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React, { useState } from 'react';
 import './App.css';
 
+function calc(equationString) {
+  console.log(equationString);
+  return false;
+}
+
 function App() {
   let [numberStr, setNumberStr] = useState('');
 
+  let [displayView, setDisplayView] = useState('');
+  //let [currentSign, setCurrentSign] = useState('');
+
+  let res;
+  if (displayView) {
+    const number = Number(displayView);
+    if (!isNaN(number)) {
+      if (displayView.includes('.')) {
+        res = displayView;
+      } else {
+        res = number;
+      }
+    } else {
+      res = displayView;
+    }
+  } else {
+    res = '0';
+  }
+
   return (
     <div className="calc-container">
-      <div id="display">
-        <div className="upper-display">
-          <p>{'dasdasd'}</p>
-        </div>
-        <div className="lower-display">
-          <p>{'sdasd'}</p>
-        </div>
-      </div>
+      <div id="display">{res}</div>
       <button
         onClick={() => {
           setNumberStr('');
+          setDisplayView('');
         }}
         className="button ac-button"
         id="clear"
@@ -26,6 +44,7 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}/`);
+          setDisplayView('/');
         }}
         className="button operator"
         id="divide"
@@ -35,6 +54,7 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}*`);
+          setDisplayView('X');
         }}
         className="button operator"
         id="multiply"
@@ -44,6 +64,11 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}7`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('7');
+          } else {
+            setDisplayView(`${displayView}7`);
+          }
         }}
         className="button"
         id="seven"
@@ -53,6 +78,11 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}8`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('8');
+          } else {
+            setDisplayView(`${displayView}8`);
+          }
         }}
         className="button"
         id="eight"
@@ -62,6 +92,11 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}9`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('9');
+          } else {
+            setDisplayView(`${displayView}9`);
+          }
         }}
         className="button"
         id="nine"
@@ -71,6 +106,7 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}-`);
+          setDisplayView('-');
         }}
         className="button operator"
         id="subtract"
@@ -80,6 +116,11 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}4`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('4');
+          } else {
+            setDisplayView(`${displayView}4`);
+          }
         }}
         className="button"
         id="four"
@@ -89,6 +130,11 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}5`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('5');
+          } else {
+            setDisplayView(`${displayView}5`);
+          }
         }}
         className="button"
         id="five"
@@ -98,6 +144,11 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}6`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('6');
+          } else {
+            setDisplayView(`${displayView}6`);
+          }
         }}
         className="button"
         id="six"
@@ -107,6 +158,7 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}+`);
+          setDisplayView('+');
         }}
         className="button operator"
         id="add"
@@ -116,6 +168,11 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}1`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('1');
+          } else {
+            setDisplayView(`${displayView}1`);
+          }
         }}
         className="button"
         id="one"
@@ -125,6 +182,11 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}2`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('2');
+          } else {
+            setDisplayView(`${displayView}2`);
+          }
         }}
         className="button"
         id="two"
@@ -134,18 +196,40 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}3`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('3');
+          } else {
+            setDisplayView(`${displayView}3`);
+          }
         }}
         className="button"
         id="three"
       >
         3
       </button>
-      <button onClick={() => {}} className="button operator" id="equals">
+      <button
+        onClick={() => {
+          const calculatedResult = calc(numberStr);
+
+          if (!calculatedResult) {
+            setDisplayView('Error!');
+          } else {
+            setDisplayView(calculatedResult);
+          }
+        }}
+        className="button operator"
+        id="equals"
+      >
         =
       </button>
       <button
         onClick={() => {
           setNumberStr(`${numberStr}0`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('0');
+          } else {
+            setDisplayView(`${displayView}0`);
+          }
         }}
         className="button"
         id="zero"
@@ -155,6 +239,13 @@ function App() {
       <button
         onClick={() => {
           setNumberStr(`${numberStr}.`);
+          if (isNaN(Number(displayView)) || displayView === '') {
+            setDisplayView('0.');
+          } else {
+            if (!displayView.includes('.')) {
+              setDisplayView(`${displayView}.`);
+            }
+          }
         }}
         className="button"
         id="decimal"
